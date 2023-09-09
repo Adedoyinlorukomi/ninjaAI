cf3616f978d6d6d00e23185aa44a79963e9a7a18
Adedoyinlorukomi <akindoyin800@gmail.com>
2023-09-09 09:17:17 +0100

    commit

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -95,7 +95,7 @@ const Index = () => {
                            
                         </div>
         
-                        <div className='text-[16px] lg:text-sm sm:text-xs'>
+                        <div className='text-[16px] lg:text-sm sm:text-xs flex gap-1'>
                             <span>Terms of use</span>
                             <span>|</span>
                             <span>Privacy Policy</span>
